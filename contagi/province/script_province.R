5a9964df1c2ae9aaba27dcf5186b5d177156e9ee
daniloderosa <danilo.derosa.dd@gmail.com>
2021-01-20 17:56:47 +0100

    Aggiunta dati province 19-01-2021

---
+++ contagi/province/script_province.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(lubridate)
+library(zoo)
+casi <- read.csv("https://github.com/pcm-dpc/COVID-19/raw/master/dati-province/dpc-covid19-ita-province.csv")
+
+province_fvg <- casi %>% 
+  filter(
+    denominazione_regione == "Friuli Venezia Giulia"
+  )
+
+str(province_fvg)
+
+province_fvg <- province_fvg %>% 
+  select(
+    data,
+    codice_provincia,
+    denominazione_provincia,
+    sigla_provincia,
+    totale_casi
+  )
+
+province_fvg$data <- ymd_hms(province_fvg$data)
+province_fvg$data <- as.Date(province_fvg$data)
+
+province_fvg <- province_fvg %>% 
+  filter(
+    denominazione_provincia != "In fase di definizione/aggiornamento"
+  )
+
+province_fvg <- province_fvg %>% 
+  group_by(denominazione_provincia) %>%
+  mutate(
+    casi_giornalieri = totale_casi - lag(totale_casi),
+    nuovi_positivi_media_mobile = round(rollmeanr(casi_giornalieri, k = 7, fill = 0))
+  )
+
+ieri = Sys.Date()-1
+province_fvg_ieri <- province_fvg %>% 
+  filter(
+    data == ieri
+  )
+
+write_csv(province_fvg_ieri, file = paste0("data/province_fvg_", ieri, ".csv"))
+write_csv(province_fvg, file = "data/province_fvg_complessivo.csv")
